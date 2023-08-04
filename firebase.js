6e78bb0dd002b3a6608443abe454b7719fbd5e40
DEVESH_JUYAL <91122298+DEVESHJUYAL@users.noreply.github.com>
2023-08-04 22:38:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBACH21P1sr8ote5xwX50LPsYTRul8PapQ",
-  authDomain: "instagramclone-e6ffb.firebaseapp.com",
-  projectId: "instagramclone-e6ffb",
-  storageBucket: "instagramclone-e6ffb.appspot.com",
-  messagingSenderId: "196235756887",
-  appId: "1:196235756887:web:abf553439cfd7d0113aa12"
 };
 
 const app = initializeApp(firebaseConfig);
